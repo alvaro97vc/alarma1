9df0e8be62ed383658d23494efd020137a7c3654
alvaro97vc <alvaro97vc@gmail.com>
2023-05-02 22:37:58 +0200

    Update alarma.js

--- alarma.js
+++ alarma.js
@@ -9,21 +9,20 @@ const TOKEN = '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';
 // Configuración para poder usar archivos estáticos en la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware para verificar el token en el método GET
-app.use((req, res, next) => {
-    const token = req.query.token;
-    if (token !== 'TOKEN_CORRECTO') {
-      res.status(401).send('Acceso no autorizado');
-      return;
-    }
-    next();
-  });
-  
-
 // Manejador para la ruta principal (GET '/')
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+    // Obtener el token del header de la petición
+    const token = req.headers['authorization'];
+    
+    // Comprobar si el token es válido
+    if (token !== 'TU_TOKEN') {
+      return res.status(401).send('Acceso no autorizado');
+    }
+    
+    // Si el token es válido, enviar el archivo HTML
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
 
 // Manejador para la ruta de alarma (POST '/alarma')
 app.post('/alarma', (req, res) => {
