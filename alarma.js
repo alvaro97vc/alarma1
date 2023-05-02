e4fc55c7bf443be70bb5927155068eba80ed9378
alvaro97vc <alvaro97vc@gmail.com>
2023-05-02 22:35:53 +0200

    Update alarma.js

--- alarma.js
+++ alarma.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const path = require('path');
 const { exec } = require('child_process');
-const jwt = require('express-jwt');
 
 const app = express();
 const port = 3000;
+const TOKEN = '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';
 
 // Configuración para poder usar archivos estáticos en la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware para validar el token
-const validarToken = jwt({ secret: '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' });
+// Middleware para verificar el token en el método GET
+app.use((req, res, next) => {
+    const token = req.query.token;
+    if (token !== 'TOKEN_CORRECTO') {
+      res.status(401).send('Acceso no autorizado');
+      return;
+    }
+    next();
+  });
+  
 
 // Manejador para la ruta principal (GET '/')
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-  exec('start alarma1.mp3');
 });
 
 // Manejador para la ruta de alarma (POST '/alarma')
-app.post('/alarma', validarToken, (req, res) => {
-  res.send('¡Alarma sonando!');
+app.post('/alarma', (req, res) => {
+  const receivedToken = req.body.token;
+  if (receivedToken === TOKEN) {
+    res.send('¡Alarma sonando!');
+    exec('start alarma1.mp3');
+  } else {
+    res.status(401).send('Token incorrecto');
+  }
 });
 
 // Inicio del servidor
