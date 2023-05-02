d1a1718f3b1301b66322597b0fd022c8dda0b511
alvaro97vc <alvaro97vc@gmail.com>
2023-05-02 22:49:32 +0200

    Update alarma.js

--- alarma.js
+++ alarma.js
@@ -4,36 +4,25 @@ const { exec } = require('child_process');
 
 const app = express();
 const port = 3000;
-const TOKEN = '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';
 
 // Configuración para poder usar archivos estáticos en la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Manejador para la ruta principal (GET '/')
 app.get('/', (req, res) => {
-  // Verificar si el token es correcto
-  const reqToken = req.query.token;
-  if (reqToken === TOKEN) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  } else {
-    console.log("OK");
-    res.status(401).send('Unauthorized');
-  }
+  res.sendFile(path.join(__dirname, 'index.html'));
+  exec('start alarma1.mp3');
+
 });
-  
 
 // Manejador para la ruta de alarma (POST '/alarma')
 app.post('/alarma', (req, res) => {
-  const receivedToken = req.body.token;
-  if (receivedToken === TOKEN) {
-    res.send('¡Alarma sonando!');
-    exec('start alarma1.mp3');
-  } else {
-    res.status(401).send('Token incorrecto');
-  }
+  res.send('¡Alarma sonando! ');
 });
 
 // Inicio del servidor
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
+
+
