456acb611e9a4ff9e0d01c30947905b455ced95f
alvaro97vc <alvaro97vc@gmail.com>
2023-05-02 21:16:10 +0200

    Initial commit

---
+++ alarma.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 3000;
+
+// Configuración para poder usar archivos estáticos en la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Manejador para la ruta principal (GET '/')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Manejador para la ruta de alarma (POST '/alarma')
+app.post('/alarma', (req, res) => {
+  exec('start alarma1.mp3');
+  res.send('¡Alarma sonando!');
+});
+
+// Inicio del servidor
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
